7ca137c48cf06400c27335577cf64eebeb7bd2a2
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-08 03:23:51 +0530

    ADDED: To play with two players

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,41 +3,78 @@
 echo "~~~~~~~~START GAME~~~~~~~~~"
 
 #Constant
-START=0
-NOPLAY=0
-SNAKE=1
-LADDER=2
-WINNING=100
+STARTING_POSITION=0
+WINNING_POSITION=100
+NOPLAY=1
+SNAKE=2
+LADDER=3
 
-echo "Starting position of player : $START"
-playerCurrentPosition=$START
+#Variables
+rollCount=0
+player=1
+currentPositionPlayer1=$STARTING_POSITION
+currentPositionPlayer2=$STARTING_POSITION
 
-function play() {
-	while [ $playerCurrentPosition -lt $WINNING ]
-	do
-		diceValue=$((RANDOM%6 + 1))
-		echo "Dice value is $diceValue"
+#Function show the current position of player
+function showCurrentPosition {
+	echo "Current Positions--> Player1: $currentPositionPlayer1 Player2: $currentPositionPlayer2"
+}
 
-		option=$((RANDOM % 3))
+function rollDice() {
+	diceValue=$((RANDOM%6 + 1))
+	option=$((RANDOM%3 + 1))
+}
+
+while [[ $currentPositionPlayer1 -ne $WINNING_POSITION && $currentPositionPlayer2 -ne $WINNING_POSITION ]]
+do
+	showCurrentPosition
+	rollDice
+	(( rollCount++ ))
+
+	if [[ $player -eq 1 ]]
+	then
+		tempPosition=$currentPositionPlayer1
+	else
+		tempPosition=$currentPositionPlayer2
+	fi
+
 		case $option in
 			$NOPLAY)
-				echo "No Play...";;
+				echo "No play..."
+				echo "Player $player stays at same position $tempPosition";;
 			$SNAKE)
 				echo "Snake..."
-					if [ $(($playerCurrentPosition - $diceValue)) -lt 0 ]
+					if [[ $(( $"$tempPosition" - $diceValue )) -lt $STARTING_POSITION ]]
 					then
-						playerCurrentPosition=$START
+						tempPosition=$STARTING_POSITION
+						echo "Player $player has came back to starting position"
 					else
-						((playerCurrentPosition-=$diceValue))
+						((tempPosition-=$diceValue))
+						echo "Player $player moves backward by $diceValue positions"
 					fi;;
 			$LADDER)
 				echo "Ladder..."
-					if [ $((playerCurrentPosition + $diceValue)) -le 100 ]
+					if [ $(( $"$tempPosition" + $diceValue )) -le $WINNING_POSITION ]
 					then
-						((playerCurrentPosition+=$diceValue))
+						((tempPosition+=$diceValue))
+						echo "Player $player moves forward by $diceValue positions"
+					else
+						echo "Player$player stays at same position $tempPosition"
 					fi;;
 		esac
-		echo "Current position of player : $playerCurrentPosition"
-	done
-}
-play
+		if [[ $player -eq 1 ]]
+		then
+			currentPositionPlayer1=$tempPosition
+		else
+			currentPositionPlayer2=$tempPosition
+		fi
+		player=$(( 3 - $player))
+done
+
+if [[ $currentPositionPlayer1 -eq $WINNING_POSITION ]]
+then
+	echo "Player1 Won!"
+else
+	echo "Player2 Won!"
+fi
+echo "Turn required: $rollCount"
