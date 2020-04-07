da152a298acf6cd4dc2b1d7448fc7ff2b4f3b871
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-07 17:14:58 +0530

    ADDED: Checked options No play, Ladder or Snake

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,6 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "~~~~~~~~START GAME~~~~~~~~~"
 
+#Constant
+START=0
+NOPLAY=0
+SNAKE=1
+LADDER=2
+
+echo "Starting position of player : $START"
 diceValue=$((RANDOM%6 + 1 ))
 echo "Dice value is $diceValue"
+
+function play() {
+	option=$((RANDOM%3))
+	case $option in
+		$NOPLAY)
+			echo "No Play...";;
+		$SNAKE)
+			echo "Snake..."
+			((playerCurrentPosition-=$diceValue));;
+		$LADDER)
+			echo "Ladder..."
+			((playerCurrentPosition+=$diceValue));;
+	esac
+	echo "Current position of player : $playerCurrentPosition"
+}
+play
